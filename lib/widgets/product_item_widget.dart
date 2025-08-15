0adb3b370fb12184bc3b122bf4e771a097e68092
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2025-08-15 08:57:55 +0700

    Update app label in AndroidManifest and refactor ProductItemWidget layout for improved readability

--- lib/widgets/product_item_widget.dart
+++ lib/widgets/product_item_widget.dart
@@ -29,63 +29,59 @@ class ProductItemWidget extends StatelessWidget {
               ),
             ),
             SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    item['title']!,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.grey10Color,
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item['title']!,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.grey10Color,
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    item['category']!,
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.grey4Color,
-                    ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  item['category']!,
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.grey4Color,
                   ),
-                  SizedBox(height: 12),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '\$${item['price']}',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.grey13Color,
-                        ),
+                ),
+                SizedBox(height: 12),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '\$${item['price']}',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.grey13Color,
                       ),
-                      SizedBox(width: 24),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/icon-star.svg',
-                            fit: BoxFit.scaleDown,
+                    ),
+                    SizedBox(width: 24),
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/icon-star.svg',
+                          fit: BoxFit.scaleDown,
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          item['rating'].toString(),
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.grey10Color,
                           ),
-                          SizedBox(width: 4),
-                          Text(
-                            item['rating'].toString(),
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              color: AppColors.grey10Color,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
             )
           ],
         ),
